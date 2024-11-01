ab07b68dadb613aa54ee9a910b853542780a6124
Jorge Hernandez <jhackhdez@gmail.com>
2024-11-01 12:20:38 -0500

    add models

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,25 @@
-const {io} = require('../index');
+const { io } = require("../index");
 
 // Sockets messages
 io.on("connection", (client) => {
-    console.log("Cliente conectado");
-  
-    client.on("disconnect", () => {
-      console.log("Cliente desconectado");
-    });
-  
-    // Para escuchar mensaje emitido desde cliente
-    client.on("message", (payload) => {
-      console.log("Mensaje", payload);
-  
-      // Esto emite un mesj a todos los clientes conectados
-      io.emit("message", { admin: "New message" });
-    });
+  console.log("Cliente conectado");
+
+  client.on("disconnect", () => {
+    console.log("Cliente desconectado");
   });
+
+  // Para escuchar mensaje emitido desde cliente
+  client.on("message", (payload) => {
+    console.log("Mensaje", payload);
+
+    // Esto emite un msj a todos los clientes conectados
+    io.emit("message", { admin: "New message" });
+  });
+
+  // Aquí se emite un mensaje enviado por un cliente específico
+  client.on("emitir-mensaje", (payload) => {
+    // console.log(payload);
+    // io.emit("nuevo-mensaje", payload); esto emite a TODOS!
+    client.broadcast.emit('nuevo-mensaje', payload); // esto emite a todos menos al que lo emitió
+  });
+});
